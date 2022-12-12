d167111cc98e1b164bc5f7fb1d871e0de18484de
indratama <indraaditama134.com>
2022-12-12 14:40:57 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://saya:1@cluster0.gxvgxic.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsaya
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+    count = db.bucket.count_documents({})
+    num = count + 1
+    doc = {
+        'num': num,
+        'bucket': bucket_receive,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'data saved'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST /bucket/done request!'})
+
+    
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
+
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num':int(num_receive)})
+    return jsonify({'msg': 'delete done!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
